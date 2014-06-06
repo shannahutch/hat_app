4fb247728762735ffff247e108da0e092918ae13
Githubshanna <shannahutch@gmail.com>
2014-06-06 16:27:04 -0700

    views and controller basics

--- app/controllers/hats_controller.rb
+++ app/controllers/hats_controller.rb
@@ -8,30 +8,44 @@ class HatsController < ApplicationController
   end
 
   def show
+    @hat = Hat.find_by_id(params[:id])
   end
 
   def create
-    hat_params = params.require(:hat).permit(:name, :description)
+    # hat_params = params.require(:hat).permit(:name, :description)
     @hat = Hat.new(hat_params)
-    @hat.save
-    redirect_to hats_path
+   if @hat.save
+      redirect_to hats_path, notice: "Hat created!"
+   else
+    render :new
+   end 
+    
   end
 
   def edit
+    #this id comes from the browser url 
     @hat = Hat.find_by_id(params[:id])
   end
 
   def update
-    hat = Hat.find_by_id(params[:id])
-    hat = params.require(:hat).permit(:name, :description)
-    if hat.update(hat)
+    @hat = Hat.find(params[:id])
+    # hat = params.require(:hat).permit(:name, :description)
+    if @hat.update(hat_params)
       redirect_to hats_path
     else
-      redirect_to edit_hat_path(hat)
+      # redirect_to edit_hat_path(@hat)
+      #rendering maintains error messages
+      render :edit
     end
   end
 
   def destroy
-    
+    @hat = Hat.find(params[:id])
+      @hat.destroy
+      redirect_to root_path
+  end
+
+  def hat_params
+    params.require(:hat).permit(:name, :description)
   end
 end
